9100d9e6f7b25152e0565b58e0faa3943c698d4a
developer <developer@developer.com>
2018-09-06 13:08:16 +0300

    init commit empty project

---
+++ app/src/main/java/developer/com/photos/MainActivity.kt
@@ -0,0 +1,12 @@
+package developer.com.photos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
